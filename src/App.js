60748a919ca59b82b4a5a7a866de1b78e42baaf7
Burak Kosova <burak.kosova@bahcesehir.edu.tr>
2022-04-04 02:37:24 +0300

    fetch with current date

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const today = new Date();
+  let date = today.toISOString().slice(0, 10);
+  console.log(date);
+
+  useEffect(() => {
+    fetch(
+      `https://seffaflik.epias.com.tr/transparency/service/market/intra-day-trade-history?endDate=${date}&startDate=${date}`,
+      {
+        method: "GET",
+        // mode: "no-cors",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        headers: {
+          "Content-Type": "application/xml",
+        },
+      }
+    )
+      .then((respone) => console.log(respone))
+      // .then((response) => response.json())
+      .catch((err) => console.log(err));
+  });
+
+  return <div></div>;
 }
 
 export default App;
